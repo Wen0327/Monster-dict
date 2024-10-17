e8f5ca37febc934b0e46fedf80252d4a0f14ca56
edison.chen <edison.chen@prophetdigits.com>
2024-10-18 01:42:14 +0800

    fix: build webPack

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/index.js',  // 项目的入口文件
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'build'),
   },
   module: {
     rules: [
